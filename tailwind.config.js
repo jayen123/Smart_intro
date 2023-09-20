a7ec69a7353d0ab609be0ffa404d24f31bcc78ba
jayen123 <jayenhasan2003@gmail.com>
2023-09-20 21:52:58 +0600

    home page 1st 4 section complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,11 +12,13 @@ module.exports = {
       colors: {
         transparent: 'transparent',
         current: 'currentColor',
-        'primary': 'linear-gradient(135deg, #F81F01 0%, #EE076E 100%)',
+        // 'primary': 'linear-gradient(135deg, #F81F01 0%, #EE076E 100%)',
+        'lpink': '#EE076E',
         'lred': '#F81F01',
         'dblack': '#101010',
         'lblack': '#191919',
-        'lgray': '#E9ECEF'
+        'lgray': '#E9ECEF',
+        'dgray': '#CED4DA'
       },
     },
   },
