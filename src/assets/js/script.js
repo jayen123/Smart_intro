26844ca3ce769085eb8014f2cc8c73e1db936adb
jayen123 <jayenhasan2003@gmail.com>
2023-09-21 20:07:59 +0600

    video section due

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,29 +1,80 @@
 "use strict";
+let largeMenuLink = document.querySelectorAll('.link');
+
+largeMenuLink.forEach(function (element, key) {
+    element.addEventListener("click", () => {
+        element.classList.toggle("active")
+
+        largeMenuLink.forEach(function (element2, key2) {
+            if (key !== key2) {
+                element2.classList.remove('active');
+            }
+
+        });
+    })
+});
 // products carousel
 $('.owl-carousel.products-carousel').owlCarousel({
-    loop:true,
-    margin:20,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
+    loop: true,
+    margin: 20,
+    nav: true,
+    navText: ['<img src="/src/assets/images/left-arrow.svg" alt="">', '<img src="/src/assets/images/right-arrow.svg" alt="">'],
+    responsive: {
+        0: {
+            items: 1
         },
-        640:{
-            items:2
+        640: {
+            items: 2
         },
-        768:{
-            items:3
+        768: {
+            items: 3
         },
-        1024:{
-            items:4
+        1024: {
+            items: 4
         }
     }
 })
 
-let ac = document.querySelectorAll('.link');
+// subscription-carousel
+$('.owl-carousel.subscription-carousel').owlCarousel({
+    loop: true,
+    margin: 20,
+    nav: true,
+    center: true,
+    navText: ['<img src="/src/assets/images/left-arrow.svg" alt="">', '<img src="/src/assets/images/right-arrow.svg" alt="">'],
+    responsive: {
+        0: {
+            items: 1
+        },
+        640: {
+            items: 2
+        },
+        768: {
+            items: 3
+        },
+        1024: {
+            items: 3
+        }
+    }
+})
 
-ac.forEach(element => {
-    element.addEventListener("click",function(){
-       element.classList.add("active");
-    })
-});
+// videos-carousel
+$('.owl-carousel.videos').owlCarousel({
+    loop: true,
+    margin: 20,
+    nav: true,
+    navText: ['<img src="/src/assets/images/left-arrow.svg" alt="">', '<img src="/src/assets/images/right-arrow.svg" alt="">'],
+    responsive: {
+        0: {
+            items: 1
+        },
+        640: {
+            items: 2
+        },
+        1024:{
+            items : 2
+        }
+    }
+})
+
+
